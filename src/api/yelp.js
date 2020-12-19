4557da914d29bead1761c60636c489f9c7d48dd2
Vinicius MAC <vincamgreg@hotmail.com>
2020-12-19 17:38:10 -0300

    yelp api request suceffuly

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import apiKey from '../../keys/YelpApiKey';
 
-
-const apiKey = process.env.REACT_NATIVE_YELP_API_KEY;
+// const apiKey = process.env.REACT_APP_YELP_API_KEY;
+//const yelpApiKey = apiKey;
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: `Bearer ${apiKey}`
+        Authorization: 'Bearer nQi0E2ryTB3MN6i-dx5BLp69VroJCmb7kD3QWtL02tQ_oL90l_bIqItp3mYudecxB8oyeiOOmiQx824oEKLhV27ZjMpX_3pfu-F1SCAcJ_KoOOOdI6qV_qVB3f1WXXYx',
     }
 });
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchPage = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    const searchApi = async () => {
+     const response = await yelp.get('/search', {
+         params: { 
+             limit: 50,
+             term,
+             location: 'santos'
+         }
+     });
+     setResults(response.data.businesses);
+
+    }
     return(
         <View style = {styles.pageContainer}>
         <SearchBar 
         term = {term}
-         onTermChanged = {(newTerm) => setTerm(newTerm)}
-         onSubmit = { () => console.log('submit')}/>
-        <Text> search page {term}</Text>
+         onTermChanged = {setTerm}
+         onSubmit = {searchApi}/>
+        <Text> We have found { results.length} results </Text>
         
         </View>
     );
