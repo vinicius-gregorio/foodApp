4c400df8b8aa266aaf92551c9ab3f3d787fd1806
Vinicius MAC <vincamgreg@hotmail.com>
2020-12-19 17:20:33 -0300

    api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 
+const apiKey = process.env.REACT_NATIVE_YELP_API_KEY;
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer apikey'
+        Authorization: `Bearer ${apiKey}`
     }
 });
