7fd432812124cbe0595dd2b759bbc1f59fd38683
Vinicius MAC <vincamgreg@hotmail.com>
2020-12-19 16:59:26 -0300

    search bar style and started to use state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { View, Text, StyleSheet , TextInput} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
     return(
         <View style = {styles.background}>
-        <Text> search bar</Text>
+            <Feather name='search' style= {styles.searchIcon}/>
+            <TextInput placeholder='Search' style= {styles.textInputStyle}/>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     background:{
+        marginTop: 15,
         backgroundColor: '#F0EEEE',
         height: 40,
         borderRadius : 5,
         marginHorizontal: 15,
+        flexDirection: 'row',
+    },
+    textInputStyle: {
+        fontSize: 18,
+        flex: 1,
+
+    },
+    searchIcon: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15,
+
     }
 });
 export default SearchBar;
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchPage = () => {
+    const [term, setTerm] = useState('');
     return(
         <View style = {styles.pageContainer}>
-        <SearchBar/>
+        <SearchBar term = {term} onTermChange = {(newTerm) => setTerm(newTerm)}/>
         <Text> search page</Text>
         </View>
     );
