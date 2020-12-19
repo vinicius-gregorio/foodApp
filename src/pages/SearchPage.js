aafaf4cc38100f6c1a8cd772157a77b74e6cb490
Vinicius MAC <vincamgreg@hotmail.com>
2020-12-19 17:50:13 -0300

    error handling

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,37 +1,43 @@
 import React, { useState } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 
 const SearchPage = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async () => {
-     const response = await yelp.get('/search', {
-         params: { 
-             limit: 50,
-             term,
-             location: 'santos'
-         }
-     });
-     setResults(response.data.businesses);
-
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'santos'
+                }
+            });
+            setResults(response.data.businesses);
+        }
+        catch (error) {
+            setErrorMessage('Something Went Wrong');
+        }
     }
-    return(
-        <View style = {styles.pageContainer}>
-        <SearchBar 
-        term = {term}
-         onTermChanged = {setTerm}
-         onSubmit = {searchApi}/>
-        <Text> We have found { results.length} results </Text>
-        
+    return (
+        <View style={styles.pageContainer}>
+            <SearchBar
+                term={term}
+                onTermChanged={setTerm}
+                onSubmit={searchApi} />
+                {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text> We have found {results.length} results </Text>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    pageContainer:{
+    pageContainer: {
         backgroundColor: 'white',
         width: '100%',
         height: '100%',
